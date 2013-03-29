d2618982146a2957d92999c784f0c7698594c478
Eric Evans <eevans@sym-link.com>
2013-03-29 10:55:21 -0500

    add licensing

--- webapp/src/main/java/org/opennms/twissandra/webapp/internal/TweetsController.java
+++ webapp/src/main/java/org/opennms/twissandra/webapp/internal/TweetsController.java
@@ -1,3 +1,18 @@
+/*
+ * Copyright 2013, Matt Brozowski and Eric Evans
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 package org.opennms.twissandra.webapp.internal;
 
 import java.security.Principal;
@@ -5,15 +20,11 @@ import java.util.Collections;
 import java.util.Date;
 import java.util.List;
 
-import javax.swing.text.DateFormatter;
-
 import org.opennms.twissandra.api.Tweet;
 import org.opennms.twissandra.api.TweetRepository;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.format.annotation.DateTimeFormat;
-import org.springframework.format.annotation.DateTimeFormat.ISO;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.context.SecurityContextHolder;
